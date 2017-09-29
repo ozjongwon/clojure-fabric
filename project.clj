7b5fa62ba2d67e948e17fd5902816b00791053b6
Jong-won Choi <oz.jongwon.choi@gmail.com>
2017-09-29 20:20:12 +1000

    init

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject clojure-fabric "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/algo.generic "0.1.2"]
+                 [org.hyperledger.fabric-sdk-java/fabric-sdk-java "1.0.1"]])
---
+++ test/clojure_fabric/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-fabric.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-fabric.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
