6de3a65f5df4c48c2a5f809bbd14bc7482de8b40
Jong-won Choi <oz.jongwon.choi@gmail.com>
2017-10-06 10:09:38 +1100

    add codec for base64

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  ;;[org.clojure/core.cache "0.6.5"]
+                 [org.clojure/data.codec "0.1.0"]
                  [medley "1.0.0"]
                  ;;[buddy/buddy-core "1.4.0"]
                  [org.bouncycastle/bcprov-jdk15on "1.58"]
