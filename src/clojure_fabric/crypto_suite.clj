c34d53da89d58c548d5397496630787a60ce3a0e
Jong-won Choi <oz.jongwon.choi@gmail.com>
2017-10-06 19:26:26 +1100

    Implement import-key

--- src/clojure_fabric/crypto_suite.clj
+++ src/clojure_fabric/crypto_suite.clj
@@ -2,15 +2,20 @@
   (:require [clojure.data.codec.base64 :as b64])
   (:import [org.bouncycastle.crypto.generators ECKeyPairGenerator]
            [org.bouncycastle.jce ECNamedCurveTable]
-           [org.bouncycastle.jce.spec ECNamedCurveParameterSpec ECParameterSpec ECNamedCurveSpec]
+           [org.bouncycastle.jce.spec ECNamedCurveParameterSpec ECNamedCurveSpec]
            [org.bouncycastle.jce.provider BouncyCastleProvider]
            [org.bouncycastle.jcajce.provider.asymmetric.ec KeyPairGeneratorSpi$ECDSA
             BCECPrivateKey BCECPublicKey]
-           [java.security KeyPairGenerator KeyFactory SecureRandom Security])
+           [javax.crypto.spec SecretKeySpec]
+           [javax.crypto SecretKeyFactory]
+           [java.security KeyFactory KeyPairGenerator SecureRandom Security]
+           [java.security.spec ECPoint ECParameterSpec ECPublicKeySpec ECPrivateKeySpec])
   (:refer-clojure :exclude [hash]))
+
 ;;;
 ;;; http://www.bouncycastle.org/wiki/display/JA1/Elliptic+Curve+Key+Pair+Generation+and+Key+Factories
-;;;
+;;; https://security.stackexchange.com/questions/129910/ecdsa-why-do-ssh-keygen-and-java-generated-public-keys-have-different-sizes
+;;; 
 
 ;; Add provider!
 (Security/addProvider (BouncyCastleProvider.))
@@ -24,7 +29,7 @@
         “ephemeral”.
   Returns
         Result (Key): The key object"
-  [algorithm {:keys [curve ephemeral]}]
+  [{:keys [algorithm curve ephemeral]}]
   (let [generator (-> algorithm
                       name
                       (KeyPairGenerator/getInstance BouncyCastleProvider/PROVIDER_NAME))
@@ -32,7 +37,7 @@
     (.initialize generator param-specs (SecureRandom.))
     (.generateKeyPair generator)))
 ;;;
-;;; Ex - (generate-key :ECDSA :secp256r1)
+;;; Ex - (generate-key {:algorithm :ECDSA :curve :secp256r1)
 ;;; 
 
 (defprotocol IBCECKey
@@ -43,7 +48,8 @@
 
 (defn- %curve-params [params]
   ;; (.multiply g (biginteger 100000000000000000000))
-  ;; 
+  ;;
+  #_
   (zipmap [:curve :g :h :n]
           ((juxt #(.getCurve ^ECParameterSpec %) #(.getG ^ECParameterSpec %) #(.getH ^ECParameterSpec %)
                  #(.getN ^ECParameterSpec %))
@@ -99,10 +105,12 @@
   ;;    Only Go SDK implements this function(and hard to understand Go code!).
   ;;    Wait until ithere are test cases to check correctness
   ;;
-  ;; (def kp (generate-key :ECDSA :secp256r1))
+  ;; (def kp (generate-key {:algorithm :ECDSA :curve :secp256r1}))
   ;; (def pri (.getPrivate kp))
   ;; (def pub (.getPublic kp))
   ;; (derive-key pri {})
+  ;; (.String (b64/encode (.getEncoded pub)))
+  ;; (.String (b64/encode (.getEncoded pri)))
   ;; 
   (%derive-key k opts))
 
@@ -114,20 +122,21 @@
         opts (Object)
   Returns
         (Key) An instance of the Key class wrapping the raw key bytes"
-  [k-str {:keys [algorithm curve ephemeral]}]
-  ;; (def b64-str (-> pub .getEncoded b64/encode String.))
-  ;;  {algorithm :ECDSA :curve :secp256r1}
-  (let [decoded-bytes (b64/decode (.getBytes ^String k-str))
-        key-factory (-> algorithm
-                        name
-                        (KeyFactory/getInstance BouncyCastleProvider/PROVIDER_NAME))
-        param-specs (-> curve name ECNamedCurveTable/getParameterSpec)
-        point (.decodePoint (.getCurve param-specs) decoded-bytes)]
-    (.generatePublic key-factory )))
+  [k-byte-array {:keys [algorithm curve ephemeral]}]
+  (let [offset 1 ;; Assume input is 1 + 64 bytes
+        curve-name (name curve)
+        kf (KeyFactory/getInstance (name algorithm) BouncyCastleProvider/PROVIDER_NAME)
+        point (ECPoint. (java.math.BigInteger. 1 #^bytes (java.util.Arrays/copyOfRange #^bytes k-byte-array offset (+ offset 32)))
+                        (java.math.BigInteger. 1 #^bytes (java.util.Arrays/copyOfRange #^bytes k-byte-array (+ offset 32) (count k-byte-array))))
+        parameter-spec (ECNamedCurveTable/getParameterSpec curve-name)
+        spec (ECNamedCurveSpec. curve-name (.getCurve parameter-spec) (.getG parameter-spec) (.getN parameter-spec)
+                                (.getH parameter-spec) (.getSeed parameter-spec))]
+    (.generatePublic kf (ECPublicKeySpec. point spec))))
+
 
 ;;;getKey
 (defn get-key
-  "Returns the key this CSP associates to the Subject Key Identifier ski.
+  "Returns the key this CSP(Crypto Service Provider) associates to the Subject Key Identifier ski.
   Params
         ski (byte[])
   Returns
@@ -197,4 +206,79 @@
         (bool): verification successful or not"
   [key signature digest])
 
+#_
+(comment
+  ;; Copy from JS SDK
+  (def test-msg "this is a test message")
+  (def test-long-msg
+    (str "The Hyperledger project is an open source collaborative effort created to advance cross-industry blockchain technologies. "
+         "It is a global collaboration including leaders in finance, banking, Internet of Things, supply chains, manufacturing and Technology. The Linux "
+         "Foundation hosts Hyperledger as a Collaborative Project under the foundation. Why Create the Project? Not since the Web itself has a technology "
+         "promised broader and more fundamental revolution than blockchain technology. A blockchain is a peer-to-peer distributed ledger forged by consensus, "
+         "combined with a system for “smart contracts” and other assistive technologies. Together these can be used to build a new generation of transactional "
+         "applications that establishes trust, accountability and transparency at their core, while streamlining business processes and legal constraints. "
+         "Think of it as an operating system for marketplaces, data-sharing networks, micro-currencies, and decentralized digital communities. It has the potential "
+         "to vastly reduce the cost and complexity of getting things done in the real world. Only an Open Source, collaborative software development approach can "
+         "ensure the transparency, longevity, interoperability and support required to bring blockchain technologies forward to mainstream commercial adoption. That "
+         "is what Hyperledger is about – communities of software developers building blockchain frameworks and platforms."))
+
+  (def hash-msg-sha384 "6247065855a812ecd182476576c02d46a675845ef4b0056e973ca42dcf8191d3adabc8c6c4b909f20f96136032ab723a")
+  (def hash-long-msg-sha384 "e647ea97fec64412a34f522b5d80cbba9a293f89d4dc63802c79bf485078ecbaed59a0d53cd7ab08a9ae983e64f886a6")
+  (def hash-msg-sha3-384 "9e9c2e5edf6cbc0b512807a8efa2917daff71b83e04dee28fcc00b1a1dd935fb5afc5eafa06bf55bd64792a597e2a8f3")
+  (def hash-long-msg-sha3-384 "47a90d6721523682e09b81da0a60e6ee1faf839f0503252316638daf038cf682c0a842edaf310eb0f480a2e181a07af0")
+  (def hash-msg-sha256 "4e4aa09b6d80efbd684e80f54a70c1d8605625c3380f4cb012b32644a002b5be")
+  (def hash-long-msg-sha256 "0d98987f5e4e3ea611f0e3d768c594ff9aac25404265d73554d12c86d7f6fbbc")
+  (def hash-msg-sha3-256 "7daeff454f7e91e3cd2d1c1bd5fcd1b6c9d4d5fffc6c327710d8fae7b06ee4a3")
+  (def hash-long-msg-sha3-256 "577174210438a85ae4311a62e5fccf2441b960013f5691993cdf38ed6ba0c84f")
+
+  (def test-key-private "93f15b31e3c3f3bddcd776d9219e93d8559e31453757b79e193a793cbd239573")
+  (def test-key-public "04f46815aa00fe2ba2814b906aa4ef1755caf152658de8997a6a858088296054baf45b06b2eba514bcbc37ae0c0cc7465115d36429d0e0bff23dc40e3760c10aa9")
+  (def test-msg-signature-sha2-256 "3046022100a6460b29373fa16ee96172bfe04666140405fdef78182280545d451f08547736022100d9022fe620ceadabbef1714b894b8d6be4b74c0f9c573bd774871764f4f789c9")
+  (def test-long-msg-signature-sha2-256 "3045022073266302d730b07499aabd0f88f12c8749a0f90144034dbc86a8cd742722ad29022100852346f93e50911008ab97afc452f83c5985a19fa3aa6d58f615c03bddaa90a1")
+
+  (def test-cert-pem
+    (str "-----BEGIN CERTIFICATE-----"
+"MIIDVDCCAvqgAwIBAgIBATAKBggqhkjOPQQDAjBOMRMwEQYDVQQKDArOoyBBY21l"
+"IENvMRkwFwYDVQQDExB0ZXN0LmV4YW1wbGUuY29tMQ8wDQYDVQQqEwZHb3BoZXIx"
+"CzAJBgNVBAYTAk5MMB4XDTE2MTIxNjIzMTAxM1oXDTE2MTIxNzAxMTAxM1owTjET"
+"MBEGA1UECgwKzqMgQWNtZSBDbzEZMBcGA1UEAxMQdGVzdC5leGFtcGxlLmNvbTEP"
+"MA0GA1UEKhMGR29waGVyMQswCQYDVQQGEwJOTDBZMBMGByqGSM49AgEGCCqGSM49"
+"AwEHA0IABFKnXh7hBdp6s9OJ/aadigT1z2WzBbSc7Hzb3rkaWFz4e+9alqqWg9lr"
+"ur/mDYzG9dudC8jFjVa7KIh+2BxgBayjggHHMIIBwzAOBgNVHQ8BAf8EBAMCAgQw"
+"JgYDVR0lBB8wHQYIKwYBBQUHAwIGCCsGAQUFBwMBBgIqAwYDgQsBMA8GA1UdEwEB"
+"/wQFMAMBAf8wDQYDVR0OBAYEBAECAwQwDwYDVR0jBAgwBoAEAQIDBDBiBggrBgEF"
+"BQcBAQRWMFQwJgYIKwYBBQUHMAGGGmh0dHA6Ly9vY0JDQ1NQLmV4YW1wbGUuY29t"
+"MCoGCCsGAQUFBzAChh5odHRwOi8vY3J0LmV4YW1wbGUuY29tL2NhMS5jcnQwRgYD"
+"VR0RBD8wPYIQdGVzdC5leGFtcGxlLmNvbYERZ29waGVyQGdvbGFuZy5vcmeHBH8A"
+"AAGHECABSGAAACABAAAAAAAAAGgwDwYDVR0gBAgwBjAEBgIqAzAqBgNVHR4EIzAh"
+"oB8wDoIMLmV4YW1wbGUuY29tMA2CC2V4YW1wbGUuY29tMFcGA1UdHwRQME4wJaAj"
+"oCGGH2h0dHA6Ly9jcmwxLmV4YW1wbGUuY29tL2NhMS5jcmwwJaAjoCGGH2h0dHA6"
+"Ly9jcmwyLmV4YW1wbGUuY29tL2NhMS5jcmwwFgYDKgMEBA9leHRyYSBleHRlbnNp"
+"b24wCgYIKoZIzj0EAwIDSAAwRQIgcguBb6FUxO+X8DbY17gpqSGuNC4NT4BddPg1"
+"UWUxIC0CIQDNyHQAwzhw+512meXRwG92GfpzSBssDKLdwlrqiHOu5A=="
+"-----END CERTIFICATE-----"))
+
+  (def test-key-private-pem
+    (str "-----BEGIN PRIVATE KEY-----"
+"MIGHAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBG0wawIBAQQgZYMvf3w5VkzzsTQY"
+"I8Z8IXuGFZmmfjIX2YSScqCvAkihRANCAAS6BhFgW/q0PzrkwT5RlWTt41VgXLgu"
+"Pv6QKvGsW7SqK6TkcCfxsWoSjy6/r1SzzTMni3J8iQRoJ3roPmoxPLK4"
+"-----END PRIVATE KEY-----"))
+  
+  (def test-key-private-cert-pem
+    (str "-----BEGIN CERTIFICATE-----"
+"MIICEDCCAbagAwIBAgIUXoY6X7jIpHAAgL267xHEpVr6NSgwCgYIKoZIzj0EAwIw"
+"fzELMAkGA1UEBhMCVVMxEzARBgNVBAgTCkNhbGlmb3JuaWExFjAUBgNVBAcTDVNh"
+"biBGcmFuY2lzY28xHzAdBgNVBAoTFkludGVybmV0IFdpZGdldHMsIEluYy4xDDAK"
+"BgNVBAsTA1dXVzEUMBIGA1UEAxMLZXhhbXBsZS5jb20wHhcNMTcwMTAzMDEyNDAw"
+"WhcNMTgwMTAzMDEyNDAwWjAQMQ4wDAYDVQQDEwVhZG1pbjBZMBMGByqGSM49AgEG"
+"CCqGSM49AwEHA0IABLoGEWBb+rQ/OuTBPlGVZO3jVWBcuC4+/pAq8axbtKorpORw"
+"J/GxahKPLr+vVLPNMyeLcnyJBGgneug+ajE8srijfzB9MA4GA1UdDwEB/wQEAwIF"
+"oDAdBgNVHSUEFjAUBggrBgEFBQcDAQYIKwYBBQUHAwIwDAYDVR0TAQH/BAIwADAd"
+"BgNVHQ4EFgQU9BUt7QfgDXx9g6zpzCyJGxXsNM0wHwYDVR0jBBgwFoAUF2dCPaqe"
+"gj/ExR2fW8OZ0bWcSBAwCgYIKoZIzj0EAwIDSAAwRQIgcWQbMzluyZsmvQCvGzPg"
+"f5B7ECxK0kdmXPXIEBiizYACIQD2x39Q4oVwO5uL6m3AVNI98C2LZWa0g2iea8wk"
+"BAHpeA=="
+"-----END CERTIFICATE-----"))
+  )
 
