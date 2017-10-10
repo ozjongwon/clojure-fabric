a09c9833745dc89fe3ba696641b6e2b44eaa2b39
Jong-won Choi <oz.jongwon.choi@gmail.com>
2017-10-10 17:39:05 +1100

    Add initial chaincode

---
+++ src/clojure_fabric/chaincode.clj
@@ -0,0 +1,61 @@
+;; http://hyperledgerdocs.readthedocs.io/en/latest/glossary.html
+;; System Chaincode
+;; System Chaincode (SCC) is a chaincode built with the peer and run in the same process as
+;; the peer. SCC is responsible for broader configurations of fabric behavior, such as timing
+;; and naming services.
+;;
+;; Lifecycle System Chaincode
+;; Lifecycle System Chaincode (LSCC) is a system chaincode that handles deployment, upgrade and
+;; termination transactions for user chaincodes.
+;;
+;; Configuration System Chaincode
+;; Configuration System Chaincode (CSCC) is a “management” system chaincode that handles
+;; configuration requests to alter an aspect of a channel (e.g. add a new member).  The CSCC will
+;; interrogate the channel’s policies to determine if a new configuration block can be created.
+;;
+;; Endorsement System Chaincode
+;; Endorsement System Chaincode (ESCC) is a system chaincode that andles the endorsement policy
+;; for specific pieces of chaincode deployed on a network, and defines the necessary parameters
+;; (percentage or combination of signatures from endorsing peers) for a transaction proposal to
+;; receive a successful proposal response (i.e. endorsement).  Deployments and invocations of
+;; user chaincodes both require a corresponding ESCC, which is defined at the time of the deployment
+;; transaction proposal for the user chaincode.
+;;
+;; Validation System Chaincode
+;; Validation System Chaincode (VSCC) Handles the validation policy for specific pieces of chaincode
+;; deployed on a network.  Deployments and invocations of user chaincodes both require a
+;; corresponding VSCC, which is defined at the time of the deployment transaction proposal for
+;; the user chaincode. VSCC validates the specified level of “endorsement” (i.e. endorsement policy)
+;; in order to prevent malicious or faulty behavior from the client.
+;;
+;; Implementation Note:
+;;      Support only CSCC, QSCC, and LSCC (from Node.js)
+(ns clojure-fabric.chaincode
+  (:require [clojure-fabric.grpc-core :as grpc]))
+
+;;; System Chaincodes
+(defonce lifecycle-system-chaincode (grpc/make-chaincode-id "lscc"))
+(defonce configuration-system-chaincode (grpc/make-chaincode-id "cscc"))
+(defonce query-system-chaincode (grpc/make-chaincode-id "qscc"))
+
+;;; System header extensions
+(defonce lscc-chaincode-header-extension
+  (grpc/make-chaincode-header-extention lifecycle-system-chaincode))
+(defonce cscc-chaincode-header-extension
+  (grpc/make-chaincode-header-extention configuration-system-chaincode))
+(defonce qscc-chaincode-header-extension
+  (grpc/make-chaincode-header-extention query-system-chaincode))
+
+;;; System chaincode map
+(defonce system-chaincodes
+  {:query-installed-chaincodes
+   {:chaincode-id lifecycle-system-chaincode
+    :header-extension lscc-chaincode-header-extension
+    :proposal-payload (grpc/make-proposal-payload lifecycle-system-chaincode
+                                                  "queryinstalledchaincodes"
+                                                  [])}
+   })
+
+(defn query-by-system-chaincode
+  [k]
+  )
