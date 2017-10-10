a544bc92f6dcaa69d92ac2677376790924f98b71
Jong-won Choi <oz.jongwon.choi@gmail.com>
2017-10-11 08:30:58 +1100

    update license info

--- src/clojure_fabric/grpc_core.clj
+++ src/clojure_fabric/grpc_core.clj
@@ -1,3 +1,18 @@
+;; Copyright 2017 Jong-won Choi <oz.jongwon.choi@gmail.com>
+;;
+;; Licensed under the Apache License, Version 2.0 (the "License")
+;; you may not use this file except in compliance with the License.
+;; You may obtain a copy of the License at
+;;
+;;     http://www.apache.org/licenses/LICENSE-2.0
+;;
+;; Unless required by applicable law or agreed to in writing, software
+;; distributed under the License is distributed on an "AS IS" BASIS,
+;; WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+;; See the License for the specific language governing permissions and
+;; limitations under the License.
+
+;;
 ;; Note that under the cover there are two different kinds of communications with the fabric backend
 ;; that trigger different events to be emitted back to the application’s handlers:
 ;; - the grpc client with the orderer service uses a “regular” stateless HTTP connection in
